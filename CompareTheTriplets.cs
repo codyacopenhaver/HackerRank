d84f557486eb8a8aa4e602ea5f2d35e01c998dbc
Cody Copenhaver <cody.a.copenhaver@gmail.com>
2017-03-16 18:56:33 -0400

    Algorithms > Warmup

---
+++ CompareTheTriplets.cs
@@ -0,0 +1,60 @@
+﻿//Challenge can be found at: https://www.hackerrank.com/challenges/compare-the-triplets
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        string[] tokens_a0 = Console.ReadLine().Split(' ');
+        int a0 = Convert.ToInt32(tokens_a0[0]);
+        int a1 = Convert.ToInt32(tokens_a0[1]);
+        int a2 = Convert.ToInt32(tokens_a0[2]);
+        string[] tokens_b0 = Console.ReadLine().Split(' ');
+        int b0 = Convert.ToInt32(tokens_b0[0]);
+        int b1 = Convert.ToInt32(tokens_b0[1]);
+        int b2 = Convert.ToInt32(tokens_b0[2]);
+
+        int alice = 0;
+        int bob = 0;
+        if (a0 == b0)
+        {
+
+        }
+        else if (a0 > b0)
+        {
+            alice++;
+        }
+        else
+        {
+            bob++;
+        }
+        if (a1 == b1)
+        {
+
+        }
+        else if (a1 > b1)
+        {
+            alice++;
+        }
+        else
+        {
+            bob++;
+        }
+        if (a2 == b2)
+        {
+
+        }
+        else if (a2 > b2)
+        {
+            alice++;
+        }
+        else
+        {
+            bob++;
+        }
+        Console.WriteLine(alice + " " + bob);
+    }
+}
